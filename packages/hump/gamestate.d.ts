0cecf2e1d125a08bd01be28ef4c4621a82f988e5
aabajyan <arsen.abajyan@pm.me>
2024-02-03 01:35:09 +0400

    Add missing compiler attribute

--- packages/hump/gamestate.d.ts
+++ packages/hump/gamestate.d.ts
@@ -1,6 +1,7 @@
 declare module "@love-packages/hump/gamestate" {
     import { Handlers } from "love-typescript-definitions/typings/love/handlers";
 
+    /** @noSelf */
     namespace gamestate {
         interface GameState extends Handlers {
             enter?: (previous: GameState, ...args: unknown[]) => void;
