aafe1d22a1c899d5d552f98c416d85ea720a71c3
aabajyan <arsen.abajyan@pm.me>
2024-02-02 22:32:26 +0400

    Remove Tove for now since we can't include dynamic libraries

--- packages/tove/init.d.ts
+++
@@ -1,7 +0,0 @@
-export type ReportLevel = "debug" | "slow" | "warn" | "err" | "fatal";
-
-export function getVersion(): string;
-
-export function setReportLevel(level: ReportLevel): void;
-
-export function getReportLevel(): ReportLevel;
