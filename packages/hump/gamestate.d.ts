416ec751a708ecc9a53e4da564198dde2130da63
aabajyan <arsen.abajyan@pm.me>
2024-02-03 01:32:54 +0400

    Hopefully fix issue when it uses colons instead of dots.

--- packages/hump/gamestate.d.ts
+++ packages/hump/gamestate.d.ts
@@ -1,24 +1,30 @@
-import { Handlers } from "love-typescript-definitions/typings/love/handlers";
+declare module "@love-packages/hump/gamestate" {
+    import { Handlers } from "love-typescript-definitions/typings/love/handlers";
 
-export interface GameState extends Handlers {
-    enter?: (previous: GameState, ...args: unknown[]) => void;
-    leave?: (...args: unknown[]) => void;
-    resume?: (...args: unknown[]) => void;
+    namespace gamestate {
+        interface GameState extends Handlers {
+            enter?: (previous: GameState, ...args: unknown[]) => void;
+            leave?: (...args: unknown[]) => void;
+            resume?: (...args: unknown[]) => void;
+        }
+
+        /** @customName switch */
+        function switchTo(to: GameState, ...args: unknown[]): void;
+
+        function push(to: GameState, ...args: unknown[]): void;
+
+        function pop(...args: unknown[]): void;
+
+        function current(): GameState;
+
+        function registerEvents(): void;
+
+        /**
+         * @customName new
+         * @deprecated
+         */
+        function newGameState(state: GameState): GameState;
+    }
+
+    export = gamestate;
 }
-
-/** @customName switch */
-export function switchTo(to: GameState, ...args: unknown[]): void;
-
-export function push(to: GameState, ...args: unknown[]): void;
-
-export function pop(...args: unknown[]): void;
-
-export function current(): GameState;
-
-export function registerEvents(): void;
-
-/**
- * @customName new
- * @deprecated
- */
-export function newGameState(state: GameState): GameState;
